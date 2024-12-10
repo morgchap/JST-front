0ecf6638bc2c29e22521b71e98f141e186143048
Morgane <m.chapeliere@mailistec.fr>
2024-12-10 17:29:50 +0100

    updating screen 3

--- screens/Signup3Screen.js
+++ screens/Signup3Screen.js
@@ -14,21 +14,28 @@ export default function Signup3Screen({navigation}) {
     const [gameName, setGameName]=useState('')
     const [gameImg, setGameImg]=useState('')
     const [gameDate, setGamedate]=useState('')
+    const [gameDescription, setGameDescription]=useState('')
+    const [gameGenre, setGameGenre]=useState('')
     const [error, setError]= useState('')
+    let searchedGame = game.replaceAll(" ", '-')
 
     const handlesubmit = ()=> {
-        fetch(`https://api.rawg.io/api/games/${game}?key=${process.env.EXPO_PUBLIC_API_KEY}`).then(response => response.json())
+        
+        fetch(`https://api.rawg.io/api/games/${searchedGame}?key=${process.env.EXPO_PUBLIC_API_KEY}`).then(response => response.json())
         .then(data => {
             // if (data.detail = 'not found.'){
             //     setError('game not found') 
             //  } else {
-                 console.log(`name : ${data.name} img : ${data.background_image} date : ${data.released}`)
+                 console.log(`name : ${data.name} img : ${data.background_image} date : ${data.released}, descr : ${data.description}, genres : ${data.genres[0]?.name}`)
                  setGame('')
                  setModalVisible(true)
                  setGameImg(data.background_image)
                  setGameName(data.name)
                  setGamedate(data.released)
                  setError('')
+                 setGameDescription(data.description)
+                 setGameGenre(data.genres.name)
+
              }     
         //}
     )
@@ -37,6 +44,9 @@ export default function Signup3Screen({navigation}) {
   return (
      <ImageBackground style={styles.image}
     source={require('../assets/background-blur.png')}> 
+    <SafeAreaView style={styles.backbutton}>
+        <FontAwesome name="chevron-left" color="#7A28CB" size={25} onPress={() => navigation.goBack()}/>
+    </SafeAreaView>
     <SafeAreaView style={styles.centered}>
     <Text style={styles.title}>Import manually</Text>
     <KeyboardAvoidingView style={styles.buttondiv}>
@@ -59,10 +69,10 @@ export default function Signup3Screen({navigation}) {
     >
     <View style={styles.modalBackground}>
         <View style={styles.modalContainer}>
+             <Image style={styles.jaquette} source={{ uri: gameImg }} />
              <Text style={styles.modalText}>{gameName}</Text>
              <Text style={styles.modalText}>{gameDate}</Text>
-             <Image style={styles.jaquette} source={{ uri: gameImg }} />
-             <TouchableOpacity>
+             <TouchableOpacity style={styles.button} onPress={()=> setModalVisible(false)}>
                 <Text>Add to my list</Text>
              </TouchableOpacity>
          </View>
@@ -98,6 +108,22 @@ const styles = StyleSheet.create({
   image:{
     flex:1,
   },  
+  button:{
+    display:'flex', 
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: "#7A28CB",
+    borderWidth:1, 
+    borderRadius:10,
+    backgroundColor: 'white',
+    height:'7%',
+    width:'80%',
+    margin:'2%',
+  },
+  backbutton:{
+    width:'90%'
+},
+
   input:{
     borderBottomColor:'#7A28CB',
     borderBottomWidth:1,
@@ -116,7 +142,8 @@ const styles = StyleSheet.create({
     borderWidth:1, 
   }, 
   backbutton:{
-    width:'90%',
+    width:'90%', 
+    marginLeft:'5%'
   }, 
   jaquette:{
 
